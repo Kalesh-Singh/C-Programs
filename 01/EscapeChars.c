e09d6d80e5b34d62e5f956b912dadc4babdf3fda
Kalesh-Singh <kaleshsingh96@gmail.com>
2017-12-01 03:58:49 -0500

    Hello world and escape chars

---
+++ 01/EscapeChars.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	printf("\tHello World!\n");
+	printf("\a\n");
+	printf("Kalesh\b\n");
+	return 0;
+}
---
+++ 01/HelloWorld.c
@@ -0,0 +1,8 @@
+/* First program */
+#include <stdio.h>
+int main()
+{
+	printf("Hello World!\n");
+	return 0;
+}
+
