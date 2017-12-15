c2cb7b4466aeb610028ad077397a6ddeeda39ed9
Kalesh-Singh <kaleshsingh96@gmail.com>
2017-12-14 20:16:26 -0500

    Two dimensional arrays

---
+++ 08/two-dimensional-arrays.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // 2D-Array with 3 rows and 5 columns
+    int marks[3][5] = {45, 56, 89, 78, 96, 74, 75, 65, 77, 65, 76, 63, 85, 49, 75};
+
+    printf("%d", marks[1][2]);
+
+    return 0;
+}
+
+    
